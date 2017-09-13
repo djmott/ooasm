6474e73f3e3d47d85e98ea4cf97cb8e0e9c8e66b
David Mott <mott.david.j@gmail.com>
2017-09-13 16:49:37 -0500

    Initial Commit

---
+++ ooasm.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "parse.hpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ parse.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <xtd/parse.hpp>
+
+namespace ooasm{
+  namespace parse{
+    namespace _{
+      STRING_(AL);
+      STRING_(AH);
+      STRING_(BL);
+      STRING_(BH);
+      STRING_(CL);
+      STRING_(CH);
+      STRING_(DL);
+      STRING_(DH);
+    }
+  }
+}
